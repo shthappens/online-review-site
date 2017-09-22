0d53422aa5c5d9490f1bfa3d3121dad2c17f97e4
Pete Eichman <peichman14@gmail.com>
2017-09-22 11:59:04 -0400

    fixed reviews

--- app/controllers/craft_beer_reviews_controller.rb
+++ app/controllers/craft_beer_reviews_controller.rb
@@ -3,26 +3,22 @@ class CraftBeerReviewsController < ApplicationController
 
   def new
     @craft_beer_review = CraftBeerReview.new
+    @craft_beer = CraftBeer.find(params[:craft_beer_id])
   end
 
   def create
-
-    if @craft_beer.nil?
-      @craft_beer = CraftBeer.new(craft_beer_params)
-    end
-
     @craft_beer_review = CraftBeerReview.new(craft_beer_review_params)
-    @craft_beer_review.user = current_user
-    # binding.pry
+    @craft_beer = CraftBeer.find(params[:craft_beer_id])
+    @craft_beer_review.craft_beer = @craft_beer
+    @user = current_user
+    @craft_beer_review.user = @user
 
-    respond_to do |format|
-      if @craft_beer_review.save
-        format.html { redirect_to @craft_beer_review, notice: 'Craft Beer Review was successfully created.' }
-        format.json { render :show, status: :created, location: @craft_beer_review  }
-      else
-        format.html { render :new }
-        format.json { render json: @craft_beer_review .errors, status: :unprocessable_entity }
-      end
+    if @craft_beer_review.save
+      flash[:notice] = "Review saved!"
+      redirect_to craft_beer_path(@craft_beer)
+    else
+      flash[:errors] = "Review didn't save :-("
+      render :new
     end
   end
 
@@ -34,14 +30,7 @@ class CraftBeerReviewsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_craft_beer_review
-      @craft_beer_review = CraftBeerReview.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
-    def craft_beer_params
-      params.require(:craft_beer_review).permit(:name, :brewery, :description)
-    end
 
     def craft_beer_review_params
       params.require(:craft_beer_review).permit(:description, :rating)
--- app/models/craft_beer.rb
+++ app/models/craft_beer.rb
@@ -1,5 +1,9 @@
 class CraftBeer < ApplicationRecord
   belongs_to :user
   has_many :craft_beer_reviews
-  # accepts_nested_attributes_for :craft_beer_reviews
+  has_many :users, through: :craft_beer_reviews
+
+  validates :name, presence: true, allow_blank: false
+  validates :brewery, presence: true, allow_blank: false
+  validates :craft_beer_type, presence:true, allow_blank: false
 end
